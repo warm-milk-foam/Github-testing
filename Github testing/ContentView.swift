4f36df9312cfae3b0f3d013dd17a1ac42d582248
Ingo <ingocodingbecauseyes@gmail.com>
2023-05-31 11:31:35 +0800

    Changed hello world to hello banana wawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawawaawawaw

--- Github testing/ContentView.swift
+++ Github testing/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("wawa")
+            Text("Hello, banana!")
         }
         .padding()
     }
