23e141efb8d01017a04c8b3e65c204c43b1b5da5
Ingo <ingocodingbecauseyes@gmail.com>
2023-05-31 11:08:46 +0800

    Initial Commit

---
+++ Github testing/Github_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_testingApp.swift
+//  Github testing
+//
+//  Created by yoeh iskandar on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
