f186ffc4512bd5851e11755964fc18ab9d38a96b
Ingo <ingocodingbecauseyes@gmail.com>
2023-05-31 11:33:34 +0800

    changed banana to potato

--- Github testing/ContentView.swift
+++ Github testing/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("wawa")
-            Text("Hello, banana!")
+            Text("Hello, potato!")
         }
         .padding()
     }
